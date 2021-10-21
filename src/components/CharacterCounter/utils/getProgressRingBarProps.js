7d46d9d6a25793e867105813b0100d1ee5e65bee
thisisshub <thisisshub@gmail.com>
2021-10-21 16:27:30 +0530

    Create What's happening? bar
    Add <TweetBox /> to home.tsx &
    This commit adds the following:
    + src/assets/*
    + src/mocks/*
    + src/pages/whatshappeningbar/*
    + src/components/CharacterCounter/*
    + src/components/Preview/*
    + src/components/PrivacyPicker/*
    + src/components/ProgressRingBar/
    + src/components/Submit/*
    + src/components/UploadMediaBar/*
    + src/components/WhatsHappeningBar/*

---
+++ src/components/CharacterCounter/utils/getProgressRingBarProps.js
@@ -0,0 +1,20 @@
+import { getColors } from "./getColors";
+
+const WARNING_AFTER = 10; // percentage to show the warning
+export const getProgressRingBarProps = (
+  plainText = "",
+  maxChars,
+  warningLimit = WARNING_AFTER
+) => {
+  const { length = 0 } = plainText;
+  const progress = Math.min((100 * length) / maxChars, 100);
+
+  const warningRange = (maxChars * warningLimit) / 100;
+  const colorBar = getColors(length, maxChars, warningRange);
+
+  const uiStatus = length >= maxChars - warningRange ? "bigRing" : "smallRing";
+  const textLabel = uiStatus === "bigRing" && `${maxChars - length}`;
+  const hideRingBar = length - maxChars > warningLimit;
+
+  return { progress, textLabel, hideRingBar, colorBar, uiStatus };
+};
