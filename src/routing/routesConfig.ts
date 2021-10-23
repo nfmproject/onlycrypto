3daada4daee5ec5b45cd83768916398aa70826dc
Souvik Mishra <mishra.souvik911@gmail.com>
2021-10-24 00:25:54 +0530

    fixed spelling for signup

--- src/routing/routesConfig.ts
+++ src/routing/routesConfig.ts
@@ -2,10 +2,10 @@ import { ROUTES_MAP } from './routes';
 
 const RoutesConfig = [
   {
-    path: ROUTES_MAP.singUp.path,
-    key: ROUTES_MAP.singUp.key,
+    path: ROUTES_MAP.signUp.path,
+    key: ROUTES_MAP.signUp.key,
     exact: true,
-    component: ROUTES_MAP.singUp.component,
+    component: ROUTES_MAP.signUp.component,
   },
   {
     path: ROUTES_MAP.home.path,
@@ -31,7 +31,7 @@ const SubRoutesConfig = [
   {
     path: '/1',
     key: 'settings/1',
-    component: ROUTES_MAP.singUp.component,
+    component: ROUTES_MAP.signUp.component,
   },
   {
     path: '/2',
