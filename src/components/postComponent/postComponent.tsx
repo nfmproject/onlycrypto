8cc3aa5b4a1f30f1953f8dc5249c44de005d96a3
user <zchoudhary.10@gmail.com>
2021-10-27 02:22:30 +0530

    fix broken postrender

--- src/components/postComponent/postComponent.tsx
+++ src/components/postComponent/postComponent.tsx
@@ -4,13 +4,13 @@ import PostList from './postList';
 
 function PostComponent({ ...props }: { postHash: string }) {
   const ceramic = CeramicAuth();
-  const [content, setContent] = useState('');
+  const [content, setContent] = useState(JSON.stringify({ username: 'loading' }));
 
   ceramic.readData(props.postHash).then((val) => {
     setContent(val);
   });
 
-  return <div>{JSON.parse(content).username}</div>;
+  return <div>{JSON.parse(content)?.username}</div>;
 }
 
 export default PostComponent;
